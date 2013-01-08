d081d63fb5e9af23738685112f547181dab049e1
Daniel Mendler <mail@daniel-mendler.de>
2013-01-08 02:21:03 +0100

    remove unnecessary absolute path requires

--- lib/daybreak.rb
+++ lib/daybreak.rb
@@ -1,17 +1,10 @@
-# Daybreak, a simple dimple key value store for ruby.
-module Daybreak
-  # The root path for Daybreak
-  ROOT = File.expand_path(File.dirname(__FILE__))
-end
-
 require 'tempfile'
 require 'thread'
-require 'zlib'
 require 'fcntl'
 
-require "#{Daybreak::ROOT}/daybreak/version"
-require "#{Daybreak::ROOT}/daybreak/locking"
-require "#{Daybreak::ROOT}/daybreak/record"
-require "#{Daybreak::ROOT}/daybreak/writer"
-require "#{Daybreak::ROOT}/daybreak/reader"
-require "#{Daybreak::ROOT}/daybreak/db"
+require 'daybreak/version'
+require 'daybreak/locking'
+require 'daybreak/record'
+require 'daybreak/writer'
+require 'daybreak/reader'
+require 'daybreak/db'
