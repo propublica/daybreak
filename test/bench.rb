16437bc485a324f3c9b8321d3f31fec9b03959da
Jeff Larson <thejefflarson@gmail.com>
2013-01-12 15:18:48 -0500

    new benchmarks

--- test/bench.rb
+++ test/bench.rb
@@ -1,28 +1,75 @@
 require File.expand_path(File.dirname(__FILE__)) + '/test_helper.rb'
+require 'benchmark'
 
-describe "benchmarks" do
-  before do
-    @db = Daybreak::DB.new DB_PATH
-    1000.times {|i| @db[i] = i }
-    @db.sync
+#inspired by moneta benchmarks
+def single(instance, &blk)
+  data = uniform
+  samples = []
+  data.each do |i|
+    samples << Benchmark.measure do
+      if blk.nil?
+        instance[i] = i
+      else
+        blk.call i
+      end
+    end.real * 1000
   end
+  instance.clear
+  samples
+end
 
-  bench_performance_constant "keys with sync" do |n|
-    n.times {|i| @db[i] = 'i'; @db.sync }
-  end
+def multi(instance, &blk)
+  data = uniform
 
-  bench_performance_constant "inserting keys" do |n|
-    n.times {|i| @db[i] = 'i' * i }
-  end
+  samples = Benchmark.measure do
+    data.each do |i|
+      if blk.nil?
+        instance[i] = i
+      else
+        blk.call i
+      end
+    end
+  end.real * 1000
+  instance.clear
+  samples
+end
 
-  bench_performance_constant "reading keys" do |n|
-    n.times {|i| assert_equal i % 1000, @db[i % 1000] }
-  end
-
-  after do
-    @db.clear
-    @db.close
-    File.unlink(DB_PATH)
+DICT = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".freeze
+def uniform
+  min, max = 3, 1024
+  1000.times.map do
+    n = rand(max - min) + max
+    (1..n).map { DICT[rand(DICT.length)] }.join
   end
 end
 
+def run(instance, message = '', &blk)
+  puts "Running benchmarks for #{instance.class.name} #{message}"
+  single instance, &blk
+  report single(instance, &blk)
+  multi instance, &blk
+  report multi(instance, &blk)
+end
+
+def report(samples)
+  if Array === samples
+    samples.sort!
+    total  = samples.inject(:+)
+    mean   = total / samples.length
+    stddev = Math.sqrt(samples.inject(0) {|m, s| m += (s - mean) ** 2 } / samples.length)
+    puts "#{samples.length} samples, average time: #{mean.round(4)} ms, std. dev: #{stddev.round(4)} ms"
+    puts "95% < #{samples.slice((samples.length * 0.95).to_i).round(4)} ms"
+  else
+    puts "Total time: #{samples.round(4)} ms"
+    puts "=" * 64
+  end
+end
+
+run Hash.new
+db = Daybreak::DB.new DB_PATH
+run db
+run db, 'with sync' do |i|
+  db[i] = i
+  db.sync
+end
+db.close
