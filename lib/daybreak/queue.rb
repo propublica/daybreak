be5326d0471d11459053f6a765196edb62a41373
Jeff Larson <thejefflarson@gmail.com>
2013-01-12 15:37:23 -0500

    uncomment optimization (sorry) and speed up benchmarks

--- lib/daybreak/queue.rb
+++ lib/daybreak/queue.rb
@@ -3,73 +3,73 @@ module Daybreak
   # global interpreter lock and makes the @queue array
   # thread safe.
   if !defined?(RUBY_ENGINE) || RUBY_ENGINE == 'ruby'
-  #   class Queue
-  #     def initialize
-  #       @queue, @full, @empty = [], [], []
-  #       # Check threads 10 times per second to avoid deadlocks
-  #       # since there is a race condition below
-  #       @stop = false
-  #       @heartbeat = Thread.new do
-  #         until @stop
-  #           unless @full.empty? || @empty.empty?
-  #             warn 'Daybreak queue: Deadlock detected'
-  #             @full.each(&:wakeup)
-  #             @empty.each(&:wakeup)
-  #           end
-  #           sleep 0.1
-  #         end
-  #       end
-  #     end
+    class Queue
+      def initialize
+        @queue, @full, @empty = [], [], []
+        # Check threads 10 times per second to avoid deadlocks
+        # since there is a race condition below
+        @stop = false
+        @heartbeat = Thread.new do
+          until @stop
+            unless @full.empty? || @empty.empty?
+              warn 'Daybreak queue: Deadlock detected'
+              @full.each(&:wakeup)
+              @empty.each(&:wakeup)
+            end
+            sleep 0.1
+          end
+        end
+      end
 
-  #     def <<(x)
-  #       @queue << x
-  #       thread = @full.shift
-  #       thread.wakeup if thread
-  #     end
+      def <<(x)
+        @queue << x
+        thread = @full.shift
+        thread.wakeup if thread
+      end
 
-  #     def pop
-  #       @queue.shift
-  #       if @queue.empty?
-  #         thread = @empty.shift
-  #         thread.wakeup if thread
-  #       end
-  #     end
+      def pop
+        @queue.shift
+        if @queue.empty?
+          thread = @empty.shift
+          thread.wakeup if thread
+        end
+      end
 
-  #     def next
-  #       while @queue.empty?
-  #         begin
-  #           @full << Thread.current
-  #           if @queue.empty?
-  #             # If a push happens here, the thread won't be woken up
-  #             Thread.stop
-  #           end
-  #         ensure
-  #           @full.delete(Thread.current)
-  #         end
-  #       end
-  #       @queue.first
-  #     end
+      def next
+        while @queue.empty?
+          begin
+            @full << Thread.current
+            if @queue.empty?
+              # If a push happens here, the thread won't be woken up
+              Thread.stop
+            end
+          ensure
+            @full.delete(Thread.current)
+          end
+        end
+        @queue.first
+      end
 
-  #     def flush
-  #       until @queue.empty?
-  #         begin
-  #           @empty << Thread.current
-  #           unless @queue.empty?
-  #             # If a pop happens here, the thread won't be woken up
-  #             Thread.stop
-  #           end
-  #         ensure
-  #           @empty.delete(Thread.current)
-  #         end
-  #       end
-  #     end
+      def flush
+        until @queue.empty?
+          begin
+            @empty << Thread.current
+            unless @queue.empty?
+              # If a pop happens here, the thread won't be woken up
+              Thread.stop
+            end
+          ensure
+            @empty.delete(Thread.current)
+          end
+        end
+      end
 
-  #     def stop
-  #       @stop = true
-  #       @heartbeat.join
-  #     end
-  #   end
-  # else
+      def stop
+        @stop = true
+        @heartbeat.join
+      end
+    end
+  else
     class Queue
       def initialize
         @mutex = Mutex.new
