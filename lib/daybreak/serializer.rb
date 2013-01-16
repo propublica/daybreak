4b137f3dd0c5260f7780fe1e7e0ae347d79bb2e0
Daniel Mendler <mail@daniel-mendler.de>
2013-01-16 20:02:25 +0100

    add comments
    [ci skip]

--- lib/daybreak/serializer.rb
+++ lib/daybreak/serializer.rb
@@ -23,14 +23,17 @@ module Daybreak
     # Serializer which does nothing
     # @api public
     class None
+      # (see Daybreak::Serializer::Default#key_for)
       def key_for(key)
         key
       end
 
+      # (see Daybreak::Serializer::Default#dump)
       def dump(value)
         value
       end
 
+      # (see Daybreak::Serializer::Default#load)
       def load(value)
         value
       end
